7ea067b1d90f30f54482b8d349fae5f1cb7c2330
Gregor Menih <gregor@menih.si>
2018-12-03 19:18:40 +0100

    feat: add solutions from day 3

---
+++ src/day03/task1.js
@@ -0,0 +1,31 @@
+const {readFileLinesToArray} = require('../utils/readFile');
+
+// ID      Y   X    W  H
+// #1407 @ 699,840: 28x17
+const parseLine = line => {
+    const trimText = x => x.map(y => y.trim());
+    const [id, rest] = trimText(line.split('@'));
+    const [coordinates, size] = trimText(rest.split(':'));
+    const [y, x] = trimText(coordinates.split(',')).map(x => parseInt(x, 10));
+    const [w, h] = trimText(size.split('x')).map(x => parseInt(x, 10));
+
+    return {id, x, y, w, h};
+};
+const makeGrid = (w, h) => [...Array(w)].map(() => [...Array(h)].map(() => 0));
+
+const lines = readFileLinesToArray('./input.txt');
+const rects = lines.map(parseLine);
+// from observation, it seems that no rect goes beyond 1000 inches
+const grid = makeGrid(1000, 1000);
+
+rects.forEach(({x, y, w, h}) => {
+    for (let i = 0; i < h; i++) {
+        for (let j = 0; j < w; j++) {
+            grid[y + j][x + i]++;
+        }
+    }
+});
+
+const gridSum = grid.reduce((sum, row) => sum + row.reduce((inch, cell) => inch + (cell >= 2 ? 1 : 0), 0), 0);
+
+console.log(`The solution is ${gridSum}`);
---
+++ src/day03/task2.js
@@ -0,0 +1,40 @@
+const {readFileLinesToArray} = require('../utils/readFile');
+
+// ID      Y   X    W  H
+// #1407 @ 699,840: 28x17
+const parseLine = line => {
+    const trimText = x => x.map(y => y.trim());
+    const [id, rest] = trimText(line.split('@'));
+    const [coordinates, size] = trimText(rest.split(':'));
+    const [y, x] = trimText(coordinates.split(',')).map(x => parseInt(x, 10));
+    const [w, h] = trimText(size.split('x')).map(x => parseInt(x, 10));
+
+    return {id, x, y, w, h};
+};
+const makeGrid = (w, h) => [...Array(w)].map(() => [...Array(h)].map(() => 0));
+
+const lines = readFileLinesToArray('./input.txt');
+const rects = lines.map(parseLine);
+const grid = makeGrid(1000, 1000);
+
+rects.forEach(({x, y, w, h}) => {
+    for (let i = 0; i < h; i++) {
+        for (let j = 0; j < w; j++) {
+            grid[y + j][x + i]++;
+        }
+    }
+});
+
+// just make another pass over the grid
+for (let {x, y, w, h, id} of rects) {
+    let allOne = true;
+    for (let i = 0; i < h && allOne; i++) {
+        for (let j = 0; j < w && allOne; j++) {
+            allOne = grid[y + j][x + i] === 1;
+        }
+    }
+    if (allOne) {
+        console.log(`And the winner is ${id}`);
+        break;
+    }
+}
