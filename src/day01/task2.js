496408af5710c352c924bdf81e8c378db717978d
Gregor Menih <gregor@menih.si>
2018-12-04 18:05:20 +0100

    fix: use set on day 1

--- src/day01/task2.js
+++ src/day01/task2.js
@@ -1,14 +1,14 @@
 const {readFileLinesToArray} = require('../utils/readFile');
 
 const lines = readFileLinesToArray('./input.txt').map(x => parseInt(x, 10));
-const rep = [];
+const rep = new Set();
 let i = 0;
 let frequency = 0;
 
 while (true) {
-    rep.push(frequency);
+    rep.add(frequency);
     frequency += lines[i % lines.length];
-    if (rep.some(x => x === frequency)) {
+    if (rep.has(frequency)) {
         break;
     }
     i++;
